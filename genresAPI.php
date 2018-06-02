bcecfb1026b085978972219a881a64a8476f65e8
pampillo <raul.pampillo@gmail.com>
2018-06-02 10:06:01 -0600

    Adding genres list API
    Adding generes list API which return genres list with amount of songs for each genre.

---
+++ genresAPI.php
@@ -0,0 +1,42 @@
+<?php
+
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+include_once('confi.php');
+
+
+//SQL query
+$sql = "
+SELECT g.name, COUNT(s.title) as songs, SUM(s.duration) as totalLength FROM `GENRES` g INNER join songs s on g.id=s.genre GROUP BY g.name;
+";
+    
+ 
+$result = $mysqli->query($sql);
+
+
+if (mysqli_connect_errno()) {
+        $response["success"] = 0;
+		$response["message"] = "Error conection";
+}else{
+   
+    if ($result->num_rows > 0) {
+        // Fetching all th
+        $array = NULL;
+        while($row = $result->fetch_assoc())
+            $array[] = $row;
+         
+         
+        $response["success"] = 1;
+        $response["data"]= $array;
+		
+		echo json_encode($response["data"]);
+		
+    }else{
+            $response["success"] = 0;
+            $response["message"] = "Error";;
+    }
+   
+    mysqli_close($mysqli);
+}
+?>
