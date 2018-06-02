2e9fdf8d8b5654c56f8a88b25c8c09fc3afb5116
pampillo <raul.pampillo@gmail.com>
2018-06-01 18:06:56 -0600

    Structure of the file
    Adding sql query, and general structure of the file (fetching the result)

--- songsAPI.php
+++ songsAPI.php
@@ -4,12 +4,36 @@ header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Ac
 header('Access-Control-Allow-Origin: *');
 include_once('confi.php');
 
+$sql = "
+SELECT s.title, s.artist, s.duration, g.name as genre FROM songs s INNER JOIN genres g WHERE g.id = s.genre;
+";
+    
+ 
+$result = $mysqli->query($sql);
+
+
 if (mysqli_connect_errno()) {
         $response["success"] = 0;
 		$response["message"] = "Error conection";
 }else{
    
+    if ($result->num_rows > 0) {
+        // Fetching all
+        $array = NULL;
+        while($row = $result->fetch_assoc())
+            $array[] = $row;
+         
+         
+        $response["success"] = 1;
+        $response["data"]= $array;
+		
+		echo json_encode($response["data"]);
+		
+    }else{
+            $response["success"] = 0;
+            $response["message"] = "Error";;
+    }
+   
     mysqli_close($mysqli);
 }
-echo json_encode($response);
 ?>
